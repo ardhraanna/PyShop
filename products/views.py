234655f51629115e2aa0efcffe36856104ff2751
ardhraanna <ardhraanna@gmail.com>
2026-07-10 10:32:52 +0530

    Initial Django PyShop project

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Products
+
+
+def index(request):
+    products = Products.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
+    return HttpResponse("Hello, world.")
+
+def new(request):
+    return HttpResponse('New Product')
+
